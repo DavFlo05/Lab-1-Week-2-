5462ff1265bfa44ab74d803fd90734d10646e1d7
DavFlo05 <davflor155@gmail.com>
2026-09-05 11:41:46 -0400

    Changing Sprite logic

--- Assets/Scripts/ChessPiece.cs
+++ Assets/Scripts/ChessPiece.cs
@@ -11,4 +11,38 @@ public enum PieceType
 public class ChessPiece : MonoBehaviour
 {
     public PieceType pieceType;
+    private SpriteRenderer spriteRenderer;
+
+    private void OnValidate()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        switch (pieceType)
+        {
+            case PieceType.Pawn:
+                spriteRenderer.sprite = pawnSprite;
+                break;
+            case PieceType.Rook:
+                spriteRenderer.sprite = rookSprite;
+                break;
+            case PieceType.Knight:
+                spriteRenderer.sprite = knightSprite;
+                break;
+            case PieceType.Bishop:
+                spriteRenderer.sprite = bishopSprite;
+                break;
+            case PieceType.Queen:
+                spriteRenderer.sprite = queenSprite;
+                break;
+            case PieceType.King:
+                spriteRenderer.sprite = kingSprite;
+                break;
+        }
+    }
+    public Sprite pawnSprite;
+    public Sprite rookSprite;
+    public Sprite knightSprite;
+    public Sprite bishopSprite;
+    public Sprite queenSprite;
+    public Sprite kingSprite;
 }
